4f7ea4f8ae149e59aaf419ea52bc94897ba634c6
chongshao223 <csha0149@uni.sydney.edu.au>
2026-09-04 18:02:29 +1000

    Add files via upload

---
+++ src/cs30/ingest/college_physics_parser.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Backward-compatible entry point for the OpenStax parser package."""
+
+from openstax_parser import main
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
